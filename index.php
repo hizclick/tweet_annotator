324ee58b2329f19212885fad739a821ad28100c1
Hizkiel Alemayehu <44091398+Hizkie@users.noreply.github.com>
2020-02-04 15:18:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -71,7 +71,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	   $response = $_POST['sentiment'];
            $id = $_POST['id'];
-	   ech $id;
+	   print("this is" .$id);
            $sql = "INSERT INTO response (tweet_id, ip, country, sentiment) VALUES (4, 3, 2 , 4)"; // insert the final result to the table called sentiment
            
 	    if($sql){
