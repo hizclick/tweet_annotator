e55269c407615557c07b4cae088bd90303fe2d68
Hizkiel Alemayehu <44091398+Hizkie@users.noreply.github.com>
2020-03-18 00:36:53 +0100

    the end is near

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <html>
 <?php
 //declaring connection variables
-    $servername = "kcpgm0ka8vudfq76.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+  $servername = "kcpgm0ka8vudfq76.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
   $username = "kcpqmduod16lyyh2";
   $password = "dahm3oxh2cakdjm8";
   $db = "vnb273g86ehntst1";
 // Create connection
 
-    $conn = new mysqli($servername, $username, $password, $db);
+  $conn = new mysqli($servername, $username, $password, $db);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$res = mysqli_query($conn,'SELECT SUM(counter) AS value_sum FROM tweet'); 
-$row = mysqli_fetch_assoc($res);
-$sum = $row['value_sum']; 
 
+	
+$res = mysqli_query($conn, "SELECT count(*) from response where ip = '".$ip."'"); 
+$count = mysqli_fetch_array($res);
+$sum =  $count[0];
 
 ?>
 
@@ -57,7 +58,7 @@ $sum = $row['value_sum'];
   
 @keyframes load {
   0% { width: 0; }
-  100% { width: <?php echo ($sum*100)/3000;?> }
+  100% { width: <?php echo ($sum*100)/300;?> }
 }
 </style>
 
