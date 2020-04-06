eed64fe241a0f1b7e84da1af965cf7a89ee5dbfe
Hizkiel Alemayehu <44091398+Hizkie@users.noreply.github.com>
2020-04-06 14:55:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -193,8 +193,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <input type="text" name="mod" class="in" id="mo" value=<?php if(isset($m)){echo $m;}?>/>
                     <label class="radio-inline" style=""><input class="radio-inline" id="pos" type="radio" name="sentiment" value="positive">አዎንታዊ</label>
                     <label class="radio-inline"><input class="radio-inline" id="neg" type="radio" name="sentiment" value="negative">አሉታዊ</label>
-                    <label class="radio-inline"><input class="radio-inline" id="neu" type="radio" name="sentiment" value="nuetral">ገለልተኛ</label><br><br><br>
-		    <label class="radio-inline"><input class="radio-inline" id="neu" type="radio" name="sentiment" value="mixed">ቅልቅል</label><br><br>
+                    <label class="radio-inline"><input class="radio-inline" id="neu" type="radio" name="sentiment" value="nuetral">ገለልተኛ</label>
+		    <label class="radio-inline"><input class="radio-inline" id="neu" type="radio" name="sentiment" value="mixed">ቅልቅል</label><br><br><br>
 		    <button type="submit" class="btn btn-lg btn-primary" name="file" id="file">መዝግብ</button><br>
                     <button type="button" class="btn btn-lg btn-danger" onclick="cls()" style="margin-left: 80%">ዝጋ</button>
 
